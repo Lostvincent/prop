868125646411d2bd9d482101c147de6dea9016dc
Vincent <lostvincent7@gmail.com>
2017-11-27 09:45:41 +0800

    rename alias

--- database/migrations/2017_11_25_145328_create_aliases_table.php
+++ database/migrations/2017_11_25_145328_create_aliases_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAliasTable extends Migration
+class CreateAliasesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateAliasTable extends Migration
      */
     public function up()
     {
-        Schema::create('alias', function (Blueprint $table) {
+        Schema::create('aliases', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('prop_id')->unsigned()->index();
             $table->string('name');
